a29349e61c4446065b354b36a65d9670c59780aa
bharath-krishnaswamy <bharathk59@gmail.com>
2020-08-26 09:27:41 +0530

    problem 4 added

--- largest_palindrome_of_3_digit_num.cpp
+++ largest_palindrome_of_3_digit_num.cpp
@@ -1,25 +1,29 @@
 #include <iostream>
+#include <stdio.h>
 
 int main()
 {
-    int max_product = 0, temp, rev = 0, pali = 0;
-    for(int i = 999; i >= 100; i--)
+    int i, j, temp, max_product = 0, rev = 0, highest = 0;
+    for(i = 999; i >= 100; i--)
     {
-        for (int j = 999;j >= i; j--)
+        for(j = 999; j >= 100; j--)
         {
-            // if(max_product  i * j)
-            //     continue;
+            if(highest > i*j)
+                continue;
             max_product = i * j;
             temp = max_product;
-            while( temp != 0 )
+            while(temp > 0)
             {
-                rev = (rev*10) + (temp%10);
-                temp /= 10;
+                rev = (rev * 10) + (temp % 10);
+                temp = temp / 10;
             }
             if(rev == max_product)
-                pali = max_product;
-            rev = 0;    
+                highest = max_product;
+            
+            // std::cout<<rev<<std::endl;
+            rev = 0;
         }
+        rev = 0;
     }
-    std::cout<<pali<<std::endl;
+    std::cout<<highest<<std::endl;
 }
